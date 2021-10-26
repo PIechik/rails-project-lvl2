61b68f58614d9bd989a99a747c5c07135ec31c9d
Polina Miftakhudinova <rambeeech@gmail.com>
2021-10-26 16:23:30 +0400

    Adding tests for likes feature

--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -3,7 +3,23 @@
 require 'test_helper'
 
 class LikesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    sign_in users(:two)
+  end
+
+  test 'can like post' do
+    post = posts(:two)
+    assert_difference('post.likes.count') do
+      post post_likes_path(post)
+    end
+  end
+
+  test 'can remove like from post' do
+    post = posts(:one)
+    assert_difference('post.likes.count', -1) do
+      delete post_likes_path(post)
+    end
+  end
 end
