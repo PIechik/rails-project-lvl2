0502932e45c5c2adccb83c276aeeb03a325575b1
Polina Miftakhudinova <rambeeech@gmail.com>
2021-12-07 21:03:08 +0400

    Changed the way nested comments are displayed

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 module PostsHelper
-  def nested_comments(comments)
-    comments.map do |comment, sub_comments|
-      render('posts/comments/comment', comment: comment) +
-        content_tag(:div, class: 'ms-4') { nested_comments(sub_comments) }
-    end.join.html_safe
-  end
-
   def user_liked_post?(post)
     PostLike.find_by(user_id: current_user&.id, post_id: post.id)
   end
