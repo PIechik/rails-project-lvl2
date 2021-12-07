878bfac24a520919fd7c0e4a114c33ac57aa5e4f
Polina Miftakhudinova <rambeeech@gmail.com>
2021-12-07 22:46:05 +0400

    Fixing rubocop offences

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
-  belongs_to :creator, class_name: 'User'
+  belongs_to :creator, class_name: 'User', inverse_of: :posts
   belongs_to :post_category
   has_many :comments, class_name: 'PostComment', dependent: :destroy
   has_many :likes, class_name: 'PostLike', dependent: :destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :posts, foreign_key: 'creator_id', dependent: :destroy
+  has_many :posts, foreign_key: 'creator_id', dependent: :destroy, inverse_of: :creator
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 end
