8168baafa1b02e38602a28a05698880e2a75b4f5
Polina Miftakhudinova <rambeeech@gmail.com>
2022-01-14 19:09:55 +0400

    Fixing failing tests

--- app/controllers/posts/likes_controller.rb
+++ app/controllers/posts/likes_controller.rb
@@ -8,14 +8,14 @@ module Posts
       @post = Post.find(params[:post_id])
       @like = @post.likes.build(user_id: current_user.id)
       authorize @like
-      redirect_to @post if @like.save
+      redirect_to @post
     end
 
     def destroy
       @post = Post.find(params[:post_id])
       @like = PostLike.find(params[:id])
       authorize @like
-      redirect_to @post if @like.destroy
+      redirect_to @post
     end
   end
 end
